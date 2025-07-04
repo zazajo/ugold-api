2cfdd3054d63c8847e0cf1a6179d923219633af2
zazajo <143228131+zazajo@users.noreply.github.com>
2025-07-04 09:19:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ CIRCULATING_SUPPLY = 3215075
 
 @app.route('/totalsupply', methods=['GET'])
 def get_total_supply():
-    return jsonify({"totalSupply": TOTAL_SUPPLY})
+    return jsonify(TOTAL_SUPPLY)
 
 @app.route('/circulatingsupply', methods=['GET'])
-def get_cmc_total_supply():
-    return jsonify({"circulatingSupply": CIRCULATING_SUPPLY})
+def get_circulating_supply():
+    return jsonify(CIRCULATING_SUPPLY)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)  # Disable debug for production
